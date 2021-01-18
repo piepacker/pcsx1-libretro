33e09937a9564532b0cba8991a1d3adc406a9ff2
Jake Stine <jake.stine@gmail.com>
2021-01-18 07:39:50 -0800

    GPU done, WORKS

--- libpcsxcore/psxbios.cpp
+++ libpcsxcore/psxbios.cpp
@@ -1850,50 +1850,69 @@ void psxBios_FlushCache() { // 44
     pc0 = ra;
 }
 
+
+#if HLE_PCSX_IFC
+#define GPU_W_DATA(dat)     GPU_writeData  (dat)
+#define GPU_W_STATUS(dat)   GPU_writeStatus(dat)
+#define GPU_R_STATUS()      GPU_readStatus ()
+#define DMA_W(addr, val)    psxHwWrite32(addr, val)
+#define DMA_R(addr)         psxHwRead32 (addr)
+#endif
+
+#if HLE_MEDNAFEN_IFC
+#define GPU_W_DATA(dat)     GPU_Write(0, 0, dat)
+#define GPU_W_STATUS(dat)   GPU_Write(0, 4, dat)
+#define GPU_R_STATUS()      GPU_Read(0, 4)
+#define DMA_W(addr, val)    DMA_Write(0, addr, val)
+#define DMA_R(addr)         DMA_Read (0, addr)
+#endif
+
+#if HLE_ENABLE_GPU
 void psxBios_GPU_dw() { // 0x46
     int size;
     s32 *ptr;
 
     PSXBIOS_LOG("psxBios_%s\n", biosA0n[0x46]);
 
-    GPU_writeData(0xa0000000);
-    GPU_writeData((a1<<16)|(a0&0xffff));
-    GPU_writeData((a3<<16)|(a2&0xffff));
+    GPU_W_DATA(0xa0000000);
+    GPU_W_DATA((a1<<16)|(a0&0xffff));
+    GPU_W_DATA((a3<<16)|(a2&0xffff));
     size = (a2*a3+1)/2;
     ptr = (s32*)PSXM(Rsp[4]);  //that is correct?
     do {
-        GPU_writeData(SWAP32(*ptr));
+        GPU_W_DATA(SWAP32(*ptr));
         ptr++;
     } while(--size);
 
     pc0 = ra;
-}
+}  
 
 void psxBios_mem2vram() { // 0x47
     int size;
 
-    GPU_writeData(0xa0000000);
-    GPU_writeData((a1<<16)|(a0&0xffff));
-    GPU_writeData((a3<<16)|(a2&0xffff));
+    GPU_W_DATA(0xa0000000);
+    GPU_W_DATA((a1<<16)|(a0&0xffff));
+    GPU_W_DATA((a3<<16)|(a2&0xffff));
     size = (a2*a3+1)/2;
-    GPU_writeStatus(0x04000002);
-    psxHwWrite32(0x1f8010f4,0);
-    psxHwWrite32(0x1f8010f0,psxHwRead32(0x1f8010f0)|0x800);
-    psxHwWrite32(0x1f8010a0,Rsp[4]);//might have a buggy...
-    psxHwWrite32(0x1f8010a4,((size/16)<<16)|16);
-    psxHwWrite32(0x1f8010a8,0x01000201);
+    GPU_W_STATUS(0x04000002);
+
+    DMA_W(0x1f8010f4,0);
+    DMA_W(0x1f8010f0, DMA_R(0x1f8010f0)|0x800);
+    DMA_W(0x1f8010a0, Rsp[4]);//might have a buggy...
+    DMA_W(0x1f8010a4, ((size/16)<<16)|16);
+    DMA_W(0x1f8010a8, 0x01000201);
 
     pc0 = ra;
 }
 
 void psxBios_SendGPU() { // 0x48
-    GPU_writeStatus(a0);
-    gpuSyncPluginSR();
+    GPU_W_STATUS(a0);
+    //gpuSyncPluginSR();
     pc0 = ra;
 }
 
 void psxBios_GPU_cw() { // 0x49
-    GPU_writeData(a0);
+    GPU_W_DATA(a0);
     pc0 = ra;
 }
 
@@ -1901,36 +1920,38 @@ void psxBios_GPU_cwb() { // 0x4a
     s32 *ptr = (s32*)Ra0;
     int size = a1;
     while(size--) {
-        GPU_writeData(SWAP32(*ptr));
+        GPU_W_DATA(SWAP32(*ptr));
         ptr++;
     }
 
     pc0 = ra;
 }
-
-void psxBios_GPU_SendPackets() { //4b:
-    GPU_writeStatus(0x04000002);
-    psxHwWrite32(0x1f8010f4,0);
-    psxHwWrite32(0x1f8010f0,psxHwRead32(0x1f8010f0)|0x800);
-    psxHwWrite32(0x1f8010a0,a0);
-    psxHwWrite32(0x1f8010a4,0);
-    psxHwWrite32(0x1f8010a8,0x010000401);
+   
+void psxBios_GPU_SendPackets() { //4b:	
+    GPU_W_STATUS(0x04000002);
+    DMA_W(0x1f8010f4, 0);
+    DMA_W(0x1f8010f0, DMA_R(0x1f8010f0)|0x800);
+    DMA_W(0x1f8010a0, a0);
+    DMA_W(0x1f8010a4, 0);
+    DMA_W(0x1f8010a8, 0x010000401);
     pc0 = ra;
 }
 
 void psxBios_sys_a0_4c() { // 0x4c GPU relate
-    psxHwWrite32(0x1f8010a8,0x00000401);
-    GPU_writeData(0x0400000);
-    GPU_writeData(0x0200000);
-    GPU_writeData(0x0100000);
+    DMA_W(0x1f8010a8,0x00000401);
+    GPU_W_DATA(0x0400000);
+    GPU_W_DATA(0x0200000);
+    GPU_W_DATA(0x0100000);
     v0 = 0x1f801814;
+
     pc0 = ra;
 }
 
 void psxBios_GPU_GetGPUStatus() { // 0x4d
-    v0 = GPU_readStatus();
+    v0 = GPU_R_STATUS();
     pc0 = ra;
 }
+#endif
 
 #undef s_addr
 
